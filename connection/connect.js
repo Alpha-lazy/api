eff325473853b22e9c30c7b70eda2f6cb7141a67
Alpha-lazy <anishmore712@gmail.com>
2024-05-06 14:11:56 +0530

    impossible

--- connection/connect.js
+++ connection/connect.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-uri ="mongodb+srv://anishmore712:Anish%402007@cluster0.u99qujo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri ="mongodb+srv://anishmore712:Anish%402007@cluster0.u99qujo.mongodb.net/"
 
 const connect = ()=>{mongoose.connect(uri)
     }
