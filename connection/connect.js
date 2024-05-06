1b859d292e1e8a79dfff945074bfcba5de79242c
Alpha-lazy <anishmore712@gmail.com>
2024-05-06 13:44:45 +0530

    let's try

--- connection/connect.js
+++ connection/connect.js
@@ -4,4 +4,13 @@ uri ="mongodb+srv://anishmore712:Anish%402007@cluster0.u99qujo.mongodb.net/songg
 const connect = ()=>{mongoose.connect(uri)
     .then(console.log("connected"))}
 
+
+
+
+
+
+
+
+
+
 module.exports = connect
