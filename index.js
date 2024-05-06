8abcc1b4f38ccb28686e0f872e806cfd4fa57614
Alpha-lazy <anishmore712@gmail.com>
2024-05-06 14:22:18 +0530

    impossible

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const port = 80
 const connect = require('./connection/connect')
 const songs = require('./Model/song')
 app.get("/" , async(req,res)=>{
+    await connect()
     res.status(200).json( await songs.findOne({Name:"295"}))
 })
 
 app.listen(port, async()=>{
     try {
-        await connect()
+        
         console.log("Server are started");
     } catch (error) {
         console.log("Internal Server error");
