80be7e35ef7807fe477ac2cab6f884a9841b168f
Alpha-lazy <anishmore712@gmail.com>
2024-05-06 15:30:05 +0530

    gggg

--- connection/connect.js
+++ connection/connect.js
@@ -2,13 +2,12 @@ const mongoose = require('mongoose')
 uri ="mongodb+srv://anishmore712:Anish%402007@cluster0.u99qujo.mongodb.net/Api"
 
 const connect = ()=>
-{mongoose.connect(
-  uri, 
-  {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-  }
-)}
+{try {
+  mongoose.connect(uri)
+  console.log("connected");
+} catch (error) {
+  console.log("error");
+}}
 
 
 
